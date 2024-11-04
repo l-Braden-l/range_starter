51ec68648d5370a32dc7c51774d160bcd194ef57
braden <leachbr787@northwested.org>
2024-11-04 14:15:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,9 @@
 #numbers = []
 #for number in range(1,1000001):
     #numbers.append(number) 
-    #print(numbers)
+ 
+    #nums_sum = sum(numbers)
+    #print(f'{nums_sum}')
 #4-6
 #for i in range(1,21,2):        #done
     #print(i)
@@ -22,8 +24,25 @@
 #for i in range(3,30):
     #print(i)
 #4-8
-cubes = [8,27,64,125,216,343,512,729,1000]
-
+cubes = []
+numbers = [0,1,2,3,4,5,6,7,8,9,10]
 for cube in cubes: 
-    for num in range(1,11):
-        print(f'The cube of {num} is {cube}')
+ for num in numbers:
+  print(f'The cube of {num} is {cube}') 
+for num in numbers:
+    # Do the math each time the loop loops (runs)
+    result = num ** 3
+    cubes.append(result)
+print('The original list of numbers:')
+print(numbers)
+print('The list of cubes:')
+print(cubes)
+
+for num in range(11):
+    # Do the math each time the loop loops (runs)
+    result = num ** 3
+    cubes.append(result)
+print('The original list of numbers:')
+print(numbers)
+print('The list of cubes:')
+print(cubes)
