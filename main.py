682df456b2335ab26ccabc578e44a12233e692ba
braden <leachbr787@northwested.org>
2024-11-04 14:16:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,25 +5,31 @@
 
 
 #4-3
-#for i in range(1,21):      #done
-    #print(i)
+for i in range(1,21):     
+    print(i)
+
 #4-4
-#for i in range(1,1000001):     #done
-    #print(i)
+    
+for i in range(1,1000001):    
+    print(i)
+
 #4-5
-#numbers = []
-#for number in range(1,1000001):
-    #numbers.append(number) 
+numbers = []
+for number in range(1,1000001):
+    numbers.append(number) 
  
-    #nums_sum = sum(numbers)
-    #print(f'{nums_sum}')
-#4-6
-#for i in range(1,21,2):        #done
-    #print(i)
+    nums_sum = sum(numbers)
+    print(f'{nums_sum}')
+
+#4-6  
+for i in range(1,21,2):       
+    print(i)
+    
 #4-7
-#for i in range(3,30):
-    #print(i)
+for i in range(3,30):
+    print(i)
 #4-8
+    
 cubes = []
 numbers = [0,1,2,3,4,5,6,7,8,9,10]
 for cube in cubes: 
