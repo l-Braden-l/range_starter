dd8d11c6d9b1310b452fb42290ae4a14c6d6de5a
braden <leachbr787@northwested.org>
2024-11-04 13:54:36 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+#Braden Leach
+# Nov 4 2024
+# Range Starter
+
+
+
+#4-3
+#for i in range(1,21):      #done
+    #print(i)
+#4-4
+#for i in range(1,1000001):     #done
+    #print(i)
+#4-5
+#numbers = []
+#for number in range(1,1000001):
+    #numbers.append(number) 
+    #print(numbers)
+#4-6
+#for i in range(1,21,2):        #done
+    #print(i)
+#4-7
+#for i in range(3,30):
+    #print(i)
+#4-8
+cubes = [8,27,64,125,216,343,512,729,1000]
+
+for cube in cubes: 
+    for num in range(1,11):
+        print(f'The cube of {num} is {cube}')
